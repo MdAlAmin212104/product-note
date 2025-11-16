27f1fdba6ad7ebc52c11cf7532ea2975009c4677
Md Al Amin <mdalamin212104@gmail.com>
2025-11-16 17:43:46 +0600

    prisma migrate data update

--- app/routes/webhooks.app.uninstalled.tsx
+++ app/routes/webhooks.app.uninstalled.tsx
@@ -1,24 +1,26 @@
 import type { ActionFunctionArgs } from "react-router";
 import { authenticate } from "../shopify.server";
 import db from "../db.server";
-import prisma from "../db.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   // 🔹 Step 1: Authenticate webhook (for uninstall event, etc.)
   const { shop, session, topic } = await authenticate.webhook(request);
   console.log(`🔔 Received ${topic} webhook for shop: ${shop}`);
-  const shopFind = await prisma.shop.findUnique({ where: { domain: shop } });
+  const shopFind = await db.shop.findUnique({ where: { domain: shop } });
 
   console.log(shopFind, "--------------------- undatall kore hyce dta ")
 
-  console.log("✅ Shop Email:", shopFind.email);
-  console.log("🏬 Shop Name:", shopFind.name);
-  console.log("🌐 Shop Domain:", shopFind.domain);
+  // 🔹 Step 2: Optional — delete shop if app uninstalled
+  if (shopFind) {
+    await db.shop.deleteMany({ where: { domain: shop } });
+    console.log(`🗑️ Deleted shop for domain: ${shop}`);
+  }
+
 
   // 🔹 Step 3: Optional — delete session if app uninstalled
   if (session) {
     await db.session.deleteMany({ where: { shop } });
-    await prisma.shop.deleteMany({ where: { domain: shop } });
+    await db.shop.deleteMany({ where: { domain: shop } });
     console.log(`🗑️ Deleted session for shop: ${shop}`);
   }
 
